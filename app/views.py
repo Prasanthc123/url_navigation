683774124e1d723c778daeab1d0166bde013f24e
Prasanth123-c <prasanthc267@gamil.com>
2023-11-20 23:17:31 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def dabba(request):
+    return render(request,'dabba.html')
+def prasanth(request):
+    return render(request,'prasanth.html')
